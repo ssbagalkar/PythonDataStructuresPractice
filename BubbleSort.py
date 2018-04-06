6c590bbe120e96e4dbd8eb93d3ceb065b1275b5b
Saurabh S Bagalkar <sbagalka@asu.edu>
2018-04-05 21:36:52 -0500

    early stopping

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,10 +1,13 @@
 def BubbleSort(myArray):
-    k = 0
     for ii in range(len(myArray)):
-        for jj in range(len(myArray)-k-1):
+        swapped = False
+        for jj in range(len(myArray)-ii-1):
             if myArray[jj] > myArray[jj+1]:
                 myArray[jj],myArray[jj+1] = myArray[jj+1],myArray[jj]
-        k =+ 1
+                swapped = True
+       
+        if swapped == False:
+           break
     return myArray
     
 arrayToBeSorted = [2,7,4,1,5,3]
