808b1177ab2f7b9af24eb44d219eec663926790b
Saurabh S Bagalkar <sbagalka@asu.edu>
2018-07-27 05:30:23 -0500

    add DFS

--- Graphs/DFS.py
+++ Graphs/DFS.py
@@ -1,44 +1,45 @@
 # Python program to print DFS traversal from a
 # given given graph
+## implementation might not be accurate
 from collections import defaultdict
- 
+
+
 # This class represents a directed graph using
 # adjacency list representation
 class Graph:
- 
-    # Constructor
-    def __init__(self):
- 
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
- 
-    # function to add an edge to graph
-    def addEdge(self,u,v):
-        self.graph[u].append(v)
- 
-    # A function used by DFS
-    def DFSUtil(self,v,visited):
- 
-        # Mark the current node as visited and print it
-        visited[v]= True
-        print (v,end = " ")
- 
-        # Recur for all the vertices adjacent to this vertex
-        for i in self.graph[v]:
-            if visited[i] == False:
-                self.DFSUtil(i, visited)
- 
- 
-    # The function to do DFS traversal. It uses
-    # recursive DFSUtil()
-    def DFS(self,v):
- 
-        # Mark all the vertices as not visited
-        visited = [False]*(len(self.graph))
- 
-        # Call the recursive helper function to print
-        # DFS traversal
-        self.DFSUtil(v,visited)
+	# Constructor
+	def __init__(self):
+		
+		# default dictionary to store graph
+		self.graph = defaultdict(list)
+	
+	# function to add an edge to graph
+	def addEdge(self, u, v):
+		self.graph[u].append(v)
+	
+	# A function used by DFS
+	def DFSUtil(self, v, visited):
+		
+		# Mark the current node as visited and print it
+		visited[v-1] = True
+		print(v, end=" ")
+		
+		# Recur for all the vertices adjacent to this vertex
+		for i in self.graph[v]:
+			if visited[i-1] == False:
+				self.DFSUtil(i, visited)
+	
+	# The function to do DFS traversal. It uses
+	# recursive DFSUtil()
+	def DFS(self, v):
+		
+		# Mark all the vertices as not visited
+		visited = [False] * (len(self.graph))
+		
+		# Call the recursive helper function to print
+		# DFS traversal
+		self.DFSUtil(v, visited)
+
 
 # Create a graph given in
 # the above diagram
@@ -49,7 +50,7 @@ g.addEdge(2, 1)
 g.addEdge(2, 4)
 g.addEdge(2, 5)
 g.addEdge(3, 1)
-g.addEdge(3, 5)	
+g.addEdge(3, 5)
 g.addEdge(4, 2)
 g.addEdge(4, 5)
 g.addEdge(4, 6)
@@ -60,7 +61,6 @@ g.addEdge(5, 6)
 g.addEdge(6, 4)
 g.addEdge(6, 5)
 
-print ("Following is Depth First Traversal"
-                  " (starting from vertex 1)")
+print("Following is Depth First Traversal"
+			" (starting from vertex 1)")
 g.DFS(1)
-		
