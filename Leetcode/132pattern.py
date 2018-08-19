85417535a743e85084b2ddd751e01e6d47190c1d
Saurabh S Bagalkar <sbagalka@asu.edu>
2018-08-18 23:42:13 -0500

    132 pattern

---
+++ Leetcode/132pattern.py
@@ -0,0 +1,14 @@
+def find132pattern(nums):
+	if sorted(nums) == nums:
+		return False
+	
+	for ii in range(len(nums) - 2):
+		for jj in range(ii+1, len(nums) - 1):
+			for kk in range(jj+1, len(nums)):
+				if nums[ii] < nums[kk] < nums[jj]:
+					return True
+	return False
+				
+print(find132pattern([5, 4, 3, 2]))
+
+#https://leetcode.com/articles/132-pattern/
