d8172563b3c2f80083c81a630649a525743dfa73
Saurabh Bagalkar <sbagalkar@alegion.com>
2020-02-27 20:03:09 -0600

    make convolution better

--- CommonQuestions/Convolution.py
+++ CommonQuestions/Convolution.py
@@ -1,26 +1,38 @@
 import numpy as np
 
-def conv2d(image, kernel):
+def conv2d(image, kernel, mode):
 	
 	# Flip the kernel in both hor and vert directions
 	kernel = np.flipud(np.fliplr(kernel))
-	output = np.zeros_like(image)
+
 	
 	# padding
-	image_padded = np.zeros_like((image.shape[0] + 2 , image.shape[1] + 2))
-	
-	image_padded[1:-1,1:-1] = image
+	if mode == 'same':
+		output = np.zeros_like(image)
+		p_x = kernel.shape[0]//2
+		p_y = kernel.shape[0]//2
+		image_padded = np.zeros((image.shape[0] + 2 * p_x, image.shape[1] + 2 * p_y))
+
+		image_padded[p_x:-p_x, p_y:-p_y] = image
+	else:
+		output = np.zeros(((image.shape[0]-kernel.shape[0])+1, (image.shape[1]-kernel.shape[1])+1))
+		image_padded = image
 	
 	#traverse
-	for x in range(image_padded.shape[1]):
-		for y in range(image_padded.shape[0]):
-			output[y,x] = (kernel * image_padded[y:y+3,x:x+3]).sum()
-			
-			
+	if mode == 'same':
+		for x in range(image.shape[0]):
+			for y in range(image.shape[1]):
+				output[y, x] = (kernel * image_padded[y:y+kernel.shape[0], x:x+kernel.shape[0]]).sum()
+
+	else:
+		# traverse
+		for x in range(output.shape[0]):
+			for y in range(output.shape[1]):
+				output[y, x] = (kernel * image_padded[y:y + kernel.shape[0], x:x + kernel.shape[0]]).sum()
+
 	return output
 	
 image = np.random.rand(5,5)
 kernel = np.ones((3,3))
 
-print(conv2d(image, kernel))
-	
+print(conv2d(image, kernel, 'valid'))
--- Udemy_11_essential_coding_questions/Section_3_Strings/non-repeating.py
+++ Udemy_11_essential_coding_questions/Section_3_Strings/non-repeating.py
@@ -1,27 +1,28 @@
 ## My solution
 from collections import OrderedDict
 from operator import itemgetter
+from collections import Counter
 
 
-def non_repeating(given_string):
-    if len(given_string) > 0:
-        my_dict = OrderedDict()
-
-        for ii in range(len(given_string)):
-            if given_string[ii] in my_dict:
-                my_dict[given_string[ii]] += 1
-            else:
-                my_dict[given_string[ii]] = 1
-
-        if 1 in my_dict.values():
-            # sort the dict
-            sorted_dict = sorted(my_dict.items(),key = itemgetter(1))
-            return sorted_dict[0][0]
-        else:
-            return None
-
-    else:
-        return None
+# def non_repeating(given_string):
+#     if len(given_string) > 0:
+#         my_dict = OrderedDict()
+#
+#         for ii in range(len(given_string)):
+#             if given_string[ii] in my_dict:
+#                 my_dict[given_string[ii]] += 1
+#             else:
+#                 my_dict[given_string[ii]] = 1
+#
+#         if 1 in my_dict.values():
+#             # sort the dict
+#             sorted_dict = sorted(my_dict.items(),key = itemgetter(1))
+#             return sorted_dict[0][0]
+#         else:
+#             return None
+#
+#     else:
+#         return None
 
 ## solution given in lesson
 # # Implement your function below.
@@ -37,6 +38,15 @@ def non_repeating(given_string):
             # return c
     # return None
 
+## My second more elegant solution
+def non_repeating(s):
+    c=Counter(s)
+    repeat=[]
+    for el in c:
+        if c[el] == 1:
+            repeat.append(el)
+            return el
+    return None
 
 print(non_repeating("abcab")) # should return 'c'
 print(non_repeating("abab")) # should return None
