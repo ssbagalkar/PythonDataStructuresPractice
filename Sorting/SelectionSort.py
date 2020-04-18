ee6e89e435a0c2e1bd980fc829d5f0c486ad8f9c
Saurabh Bagalkar <sbagalkar@alegion.com>
2020-04-18 17:11:04 -0500

    change implementation

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -1,11 +1,11 @@
-def SelectionSort(myArray):
-	minValueIndex = 0
-	for ii in range(len(myArray)):	
-		for count in range(ii+1,len(myArray)):
-			if myArray[count] < myArray[minValueIndex]:
-				minValueIndex = count
-		myArray[ii],myArray[minValueIndex] = myArray[minValueIndex],myArray[ii]
-	return myArray
+def selection_sort(arr):
+    n = len(arr)
+    for ii in range(n):
+        min_element_index = ii
+        for jj in range(ii+1, n):
+            if arr[jj] < arr[min_element_index]:
+                min_element_index = jj
+        arr[ii], arr[min_element_index] = arr[min_element_index], arr[ii]
+    return arr
 
-arrayToBeSorted = [ 32, 2, 56, 67, 30, 5, 99, 23]
-print(SelectionSort(arrayToBeSorted))
+print(selection_sort([5, 2, 0, -8, 3]))
