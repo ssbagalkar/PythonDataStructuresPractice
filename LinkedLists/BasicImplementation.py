2f83e2819cb2565cf7688e0cca193cd724866663
Saurabh Bagalkar <sbagalka@asu.edu>
2021-05-06 13:03:49 -0500

    new basic implementation with nth from last

--- LinkedLists/BasicImplementation.py
+++ LinkedLists/BasicImplementation.py
@@ -65,6 +65,29 @@ class LinkedList:
 				return
 			current_idx += 1
 
+
+	def nth_from_end(self, n):
+		#https://www.udemy.com/course/11-essential-coding-interview-questions/learn/lecture/7627022#overview
+
+		pointer_one = self.head
+		pointer_two = self.head
+
+		# Move pointer two towards the end
+		for ii in range(n):
+			if pointer_two.next is not None:
+				pointer_two = pointer_two.next
+			else:
+				return None
+
+		# Move both pointers one by one
+		while pointer_two.next is not None:
+			pointer_two = pointer_two.next
+			pointer_one = pointer_one.next
+		return pointer_one.val
+
+
+
+
 my_ll = LinkedList()
 
 my_ll.display()
@@ -74,7 +97,8 @@ my_ll.append(2)
 my_ll.append(3)
 my_ll.append(4)
 my_ll.display()
-print(my_ll.get(2))
-my_ll.erase(2)
-my_ll.display()
+# print(my_ll.get(2))
+# my_ll.erase(2)
+# my_ll.display()
+print(my_ll.nth_from_end(0))
 
