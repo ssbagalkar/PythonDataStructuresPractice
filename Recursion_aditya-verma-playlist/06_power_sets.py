f923c3da6d9347c02286fdc0ae38fe7d02dd838f
Saurabh Bagalkar <sbagalka@asu.edu>
2021-06-09 11:27:34 -0500

    generate power sets

---
+++ Recursion_aditya-verma-playlist/06_power_sets.py
@@ -0,0 +1,22 @@
+"""
+Video Link --> https://www.youtube.com/watch?v=Yg5a2FxU4Fo&list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY&index=12&ab_channel=AdityaVermaAdityaVerma
+Geeks link -->  https://practice.geeksforgeeks.org/problems/power-set-using-recursion/1/?track=sp-recursion&batchId=105
+Time complexity -- > O(2^N)
+Space Complexity --> ?
+"""
+
+
+def generate_power_set(input, output):
+	# base condition
+	if input == "":
+		print(output)
+		return
+
+	output_one = output
+	output_two = output+input[0]
+	generate_power_set(input[1:], output_one)
+	generate_power_set(input[1:], output_two)
+
+
+#driver code
+generate_power_set("abc", "")
