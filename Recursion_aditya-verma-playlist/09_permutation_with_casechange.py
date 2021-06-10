340fac0b31c0ad40918196ba0a5357f44083870a
Saurabh Bagalkar <sbagalka@asu.edu>
2021-06-10 10:22:43 -0500

    case change

---
+++ Recursion_aditya-verma-playlist/09_permutation_with_casechange.py
@@ -0,0 +1,18 @@
+"""
+https://www.youtube.com/watch?v=J2Er5XceU_I&list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY&index=15&ab_channel=AdityaVermaAdityaVerma
+"""
+
+def permutation_with_case_change(input_str, output_str):
+	# base condition
+	if len(input_str) == 0:
+		print(output_str)
+		return
+
+	# logic
+	permutation_with_case_change(input_str[1:], output_str + input_str[0].lower())
+	permutation_with_case_change(input_str[1:], output_str + input_str[0].upper())
+
+
+#driver
+input_str = "abc"
+permutation_with_case_change(input_str, "")
